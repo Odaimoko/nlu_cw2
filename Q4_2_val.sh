c8f1da57067d7c3f65050be26b10926333fb0a84
oda <odaimoko@gmail.com>
2020-03-01 05:36:38 +0800

    Q4 val update

--- Q4_2_val.sh
+++ Q4_2_val.sh
@@ -7,10 +7,11 @@ EXP_ROOT="result/Q4-9.90"
 DATA_DIR="${ROOT}/europarl_prepared"
 TEST_EN_GOLD="${ROOT}/europarl_raw/test.en"
 TEST_EN_PRED="${EXP_ROOT}/model_translations.txt"
+ckpt_name="checkpoint_best.pt"
 mkdir -p ${EXP_ROOT}
 mkdir -p ${RESULTS_ROOT}
 # perplexity and loss
-python validate.py --cuda True --restore-file checkpoint_best.pt --save-dir $EXP_ROOT --encoder-num-layers 2 --decoder-num-layers 3
+# python validate.py --cuda True --restore-file $ckpt_name --save-dir $EXP_ROOT --encoder-num-layers 2 --decoder-num-layers 3
 # translate
-python translate.py     --checkpoint-path "${EXP_ROOT}/checkpoint_best.pt"     --output "${TEST_EN_PRED}" --cuda True
-perl multi-bleu.perl -lc ${TEST_EN_GOLD} < ${TEST_EN_PRED} | tee "${EXP_ROOT}/bleu.txt"
+python translate.py --checkpoint-path "${EXP_ROOT}/$ckpt_name" --output "${TEST_EN_PRED}" --cuda True
+perl multi-bleu.perl -lc ${TEST_EN_GOLD} <${TEST_EN_PRED} | tee "${EXP_ROOT}/bleu.txt"
