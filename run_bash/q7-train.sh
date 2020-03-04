59bd38d3fdc4e9037d1b08b87014a04301574316
oda <odaimoko@gmail.com>
2020-03-05 07:12:12 +0800

    Q4-q7 train

--- run_bash/q7-train.sh
+++ run_bash/q7-train.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 # Define a location for all your experiments to save
 . /opt/anaconda3/etc/profile.d/conda.sh 
+conda activate nlu
 ROOT="."
 RESULTS_ROOT="${ROOT}/oda_exp"
 
 ### NAME YOUR EXPERIMENT HERE ##
-EXP_NAME="q4"
+EXP_NAME="q7"
 ################################
 
 ## Local variables for current experiment
 EXP_ROOT="${RESULTS_ROOT}/${EXP_NAME}"
 DATA_DIR="${ROOT}/europarl_prepared"
 TEST_EN_GOLD="${ROOT}/europarl_raw/test.en"
-TEST_EN_PRED="${EXP_ROOT}/model_translations.txt"
+TEST_EN_PRED="${EXP_ROOT}/model_translation.txt"
 mkdir -p ${EXP_ROOT}
 mkdir -p ${RESULTS_ROOT}
 
@@ -21,15 +22,14 @@ python train.py --save-dir "${EXP_ROOT}" \
                 --log-file "${EXP_ROOT}/train_log.txt"  \
                 --data "${DATA_DIR}" \
                 --max-epoch 150 \
-                --encoder-num-layers 2 --decoder-num-layers 3 --cuda True \
-                --patience 20
+                 --cuda True \
+                --arch \
+                transformer  --restore-file ".pt"
 
                 ### ADDITIONAL ARGUMENTS HERE ###
 
 ## Prediction step
-python translate.py \
-    --checkpoint-path "${EXP_ROOT}/checkpoint_best.pt" \
-    --output "${TEST_EN_PRED}" --cuda True
+python translate.py     --checkpoint-path "${EXP_ROOT}/checkpoint_best.pt"     --output "${TEST_EN_PRED}" --cuda True
 
 ## Calculate BLEU score for model outputs
 perl multi-bleu.perl -lc ${TEST_EN_GOLD} < ${TEST_EN_PRED} | tee "${EXP_ROOT}/bleu.txt"
